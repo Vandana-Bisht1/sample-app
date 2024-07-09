bfc26143725bb739f88a0440e1add2fecffc3fd1
Vandana Bisht <vandana.bisht@accolitedigital.com>
2024-07-09 22:02:45 +0530

    integrated graphql

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import client from './graphql/apolloClient';
+import Countries from './graphql/Countries';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <Countries />
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/graphql/Countries.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_COUNTRIES } from "./queries";
+import "./Countries.css";
+
+function Countries() {
+  const { loading, error, data } = useQuery(GET_COUNTRIES);
+
+  if (loading) return <p className="loading">Loading...</p>;
+  if (error) return <p className="error">Error: {error.message}</p>;
+
+  return (
+    <div className="container">
+      <h2>Countries</h2>
+      <div className="countries">
+        {data.countries.map((country) => (
+          <div key={country.code} className="country-card">
+            <h3>
+              {country.name} {country.emoji}
+            </h3>
+            <p>Capital: {country.capital}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Countries;
